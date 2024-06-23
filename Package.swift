0bc136fa8e0158694c905e5ded2c36c029a728bd
Kevin Lou <58053969+klou23@users.noreply.github.com>
2024-06-23 10:40:38 -0600

    Custom implementation of URLSession for async

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "TastyTradeAPI",
     platforms: [
-        .macOS(.v12),
-        .iOS(.v15)
+        .macOS(.v10_15),
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
---
+++ Sources/TastyTradeAPI/Util/URLSession+asyncData.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  
+//
+//  Created by Kevin Lou on 6/23/24.
+//
+
+import Foundation
+
+extension URLSession {
+    
+    enum URLSessionAsyncErrors: Error {
+        case invalidURLResponse
+        case missingResponseData
+    }
+    
+    func asyncData(for request: URLRequest) async throws -> (Data, URLResponse) {
+        return try await withCheckedThrowingContinuation { continuation in
+            let task = self.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    continuation.resume(throwing: error)
+                    return
+                }
+                guard let response = response as? HTTPURLResponse else {
+                    continuation.resume(throwing: URLSessionAsyncErrors.invalidURLResponse)
+                    return
+                }
+                guard let data = data else {
+                    continuation.resume(throwing: URLSessionAsyncErrors.missingResponseData)
+                    return
+                }
+                continuation.resume(returning: (data, response))
+            }
+            task.resume()
+        }
+    }
+    
+}
